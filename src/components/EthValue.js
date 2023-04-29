88d367846a06eef9dd76bbd4e3778e33e083c2d1
Kemal Cleva <kemalcleva69@gmail.com>
2023-04-29 21:12:45 +0200

    Final back-end
    this is the final back-end job.
    This app is now able to:
    
    Create an application that will allow a user to view transaction data from the Ethereum blockchain associated with a specific wallet address W that the user inputs, starting with block B. The application should get information on:
    
    wallets (addresses) and
    
    amounts of ETH associated with transactions made to and from the given wallet W and
    
    show them in a simple human-readable way (ideally, through a web page).
    
    The application should collect and display ALL transaction data starting from the given block B.
    
    The app is also able to:
    Given a date in YYYY-MM-DD format, the program should return the exact value of ETH that was available on the given address at YYYY-MM-DD 00:00 UTC time.

--- src/components/EthValue.js
+++ src/components/EthValue.js
@@ -2,33 +2,32 @@ import React, { useState } from "react";
 import { ethers } from "ethers";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import axios from "axios";
 
-function EthValue() {
+function EthBalance() {
   const [address, setAddress] = useState("");
   const [date, setDate] = useState(new Date());
-  const [value, setValue] = useState(0);
+  const [balance, setBalance] = useState(0);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const provider = new ethers.providers.JsonRpcProvider(
-      "https://api.etherscan.io/api"
-    );
-    const blockNumber = await provider.getBlockNumber(date);
-    const block = await provider.getBlock(blockNumber);
-    const timestamp = block.timestamp;
-    const apiKey = "ADJUI5FEAT4IST27EVZEPJCAKY9D5N4RHN";
-    const response = await axios.get(
-      `?module=account&action=balance&address=${address}&tag=latest&apikey=${apiKey}`
-    );
-    const balance = ethers.utils.parseEther(response.data.result);
 
-    setValue(ethers.utils.formatEther(balance));
+    const provider = new ethers.JsonRpcProvider(
+      "https://mainnet.infura.io/v3/64ac682fbe2746de9f7bf443b6f13f67"
+    );
+
+    const timestamp = Math.round(date.getTime() / 1000); // Convert date to timestamp in seconds
+    const blockNumber = await provider.getBlockNumber(timestamp);
+    console.log("Block number at date:", blockNumber);
+
+    const balance = await provider.getBalance(address, blockNumber);
+    const ethBalance = ethers.formatEther(balance);
+    console.log("ETH balance:", ethBalance);
+    setBalance(ethBalance);
   };
 
   return (
-    <div className="eth-value">
-      <h2>Ethereum Value Calculator</h2>
+    <div className="eth-balance">
+      <h2>Ethereum Balance Calculator</h2>
       <form onSubmit={handleSubmit}>
         <label>
           Address:
@@ -44,12 +43,12 @@ function EthValue() {
           <DatePicker selected={date} onChange={(date) => setDate(date)} />
         </label>
         <br />
-        <button type="submit">Check ETH Value</button>
+        <button type="submit">Check ETH Balance</button>
       </form>
-      <h3>ETH value on {date.toDateString()}:</h3>
-      <p>{value} ETH</p>
+      <h3>ETH balance on {date.toDateString()}:</h3>
+      <p>{balance} ETH</p>
     </div>
   );
 }
 
-export default EthValue;
+export default EthBalance;
